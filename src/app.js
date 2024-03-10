c9acf5ca5ed7df4f1f9abb19d874411757e95de2
Abhay-raj69 <rajabhay56999@gmail.com>
2024-03-11 02:50:06 +0530

    utilities code written

--- src/app.js
+++ src/app.js
@@ -1,3 +1,26 @@
 import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
 
 const app = express();
+
+app.use(
+  cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials: true,
+  })
+);
+
+// limit of the data we will accept
+app.use(express.json({ limit: "16kb" }));
+
+// data from url live % + between any url
+app.use(express.urlencoded({ extended: true ,limit:"16kb"}));
+
+// a public folder that will be used for storing photos,pdf etc
+app.use(express.static("public"))
+
+app.use(cookieParser())
+ 
+
+export { app };
--- src/index.js
+++ src/index.js
@@ -1,15 +1,27 @@
 // require('dotenv').config({path: './env'})
 
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
 dotenv.config({
-    path:'./env'
-})
-
+  path: "./env",
+});
 
 connectDB()
-
+  .then(() => {
+    // it will listen for errors before starting the server
+    app.on("error",(error)=>{
+      console.log("ERROR ",error);
+      throw error
+    })
+    // here server will start
+    app.listen(process.env.PORT || 8000,()=>{
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MongoDB connection failed !!! ", error);
+  });
 
 /*
 import express from "express";
