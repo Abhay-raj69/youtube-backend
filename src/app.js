666c931902e78929e2a93b2f24a7fc86c70e8f71
Abhay-raj69 <rajabhay56999@gmail.com>
2024-03-14 14:27:16 +0530

    user registration

--- src/app.js
+++ src/app.js
@@ -15,12 +15,21 @@ app.use(
 app.use(express.json({ limit: "16kb" }));
 
 // data from url live % + between any url
-app.use(express.urlencoded({ extended: true ,limit:"16kb"}));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 // a public folder that will be used for storing photos,pdf etc
-app.use(express.static("public"))
+app.use(express.static("public"));
 
-app.use(cookieParser())
- 
+app.use(cookieParser());
+
+// routes import
+
+import userRouter from "./routes/user.routes.js";
+
+// routes declaration
+
+app.use("/api/v1/users", userRouter);
+
+ // http://localhost:8000/api/v1/users/register
 
 export { app };
