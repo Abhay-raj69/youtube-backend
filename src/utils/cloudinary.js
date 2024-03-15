092c26095c1b584e922e696cd8f76e7b468a9d11
Abhay-raj69 <rajabhay56999@gmail.com>
2024-03-16 03:59:25 +0530

    more bug fixed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -16,10 +16,10 @@ const uploadOnCloudinary = async (localFilePath) => {
     // upload the files
     const response = await cloudinary.uploader.upload(localFilePath, {
       // it will tell us which type of file is uploaded
-      resource_type: "image",
+      resource_type: "auto",
     });
     // file has been uploaded successfully
-    console.log("File is uploaded on cloudinary", response.url);
+    fs.unlinkSync(localFilePath)
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
